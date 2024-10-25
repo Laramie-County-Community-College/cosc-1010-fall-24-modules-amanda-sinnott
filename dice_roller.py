e889a154ff49054e4dbcb2eba7631a9e83b380d3
amanda-sinnott <amandasinnott@student.lccc.wy.edu>
2024-10-25 22:27:59 +0000

    this code defines roll_die and checks that the die has 4 sides

--- dice_roller.py
+++ dice_roller.py
@@ -4,7 +4,7 @@
     #### write  your code here - make sure to return the result of your roll ###
     #### your function, called roll_die, should meet these requirements.  
     #### Include this documentation inside of your function!
-    """Rolls a die with the specified number of sides and returns the result.
+"""Rolls a die with the specified number of sides and returns the result.
 
     Args:
         sides (int): The number of sides on the die (must be at least 4).
@@ -14,7 +14,26 @@
     """
 
 
+import random
 
+def roll_die(sides):
+    if sides < 4:
+        raise ValueError("Die must have at least 4 sides.")
+    return random.randint(1, sides)
+
+def test_dice_roller():
+    for _ in range(100):
+        roll = roll_die(6)
+        assert 1 <= roll <= 6, f"Roll {roll} is out of range for a 6-sided die"
+
+    for _ in range(100):
+        roll = roll_die(10)
+        assert 1 <= roll <= 10, f"Roll {roll} is out of range for a 10 sided die"
+
+    print("All tests passed.")
+
+if __name__ == "__main__":
+    test_dice_roller()
 
 ## You should be able to run this program directly to use this unit test before moving on to the dice_stats.py
 def test_dice_roller():
@@ -30,3 +49,4 @@ def test_dice_roller():
 
 if __name__ == "__main__":
     test_dice_roller()  # Run the unit tests when executed directly
+
